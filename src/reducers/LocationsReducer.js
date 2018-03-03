308246871ce3233f3daf3fbed0ae463ce683db51
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-03 15:28:06 -0700

    Fix return statement in SAVE_LOCATION case to not use spread operator for payload

--- src/reducers/LocationsReducer.js
+++ src/reducers/LocationsReducer.js
@@ -1,10 +1,9 @@
 const Locations = (state = [], action) => {
   switch (action.type) {
     case 'STORE_LOCATIONS':
-      // store an array of locations to state
       return [...state, ...action.data];
     case 'SAVE_LOCATION':
-      return [...state.data, ...action.data];
+      return [...state, action.data];
     default:
       return state;
   }
