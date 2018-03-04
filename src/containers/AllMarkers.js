1277bb972de68fe34ccbdc9f04f58eb13e0b9844
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 13:53:41 -0700

    Dispatch modify polygon in AllMarkersContainer

--- src/containers/AllMarkers.js
+++ src/containers/AllMarkers.js
@@ -1,14 +1,15 @@
-/*eslint-disable no-unused-vars*/
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AllMarkers from '../components/AllMarkers';
+import { modifyPolygon } from '../actions';
 
 const mapStateToProps = ({ Locations }) => {
   return { locations: Locations };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    modifyPolygon: location => dispatch(modifyPolygon(location)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllMarkers);
