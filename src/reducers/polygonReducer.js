608b72578fa34cfde645ed80ec2d5df1ad5e7ea8
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 13:51:19 -0700

    Add fucntionality to get index of multidimensional array in polygonReducer

--- src/reducers/polygonReducer.js
+++ src/reducers/polygonReducer.js
@@ -1,17 +1,32 @@
+const matchLocation = (locationArray, location) => {
+  if (!locationArray.length) { return null; }
+
+  for (let i = 0; i < locationArray.length; i += 1) {
+    if (locationArray[i][0] === location[0] && locationArray[i][1] === location[1]) {
+      return i;
+    }
+  }
+  return null;
+};
+
 const modifyLocations = (state, actionData) => {
   const newState = [...state];
-  const index = newState.indexOf(actionData);
+  const index = matchLocation(newState, actionData);
 
-  if (index > -1) {
+  if (index !== null && newState.length === 1) {
+    return [];
+  }
+
+  if (index !== null) {
     return newState.splice(index, 1);
   }
 
-  return newState.push(actionData);
+  return [...newState, actionData];
 };
 
 const polygonReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TO_POLYGON':
+    case 'MODIFY_POLYGON':
       return modifyLocations(state, action.data);
     default:
       return state;
