425e643b21f26d210120937bcfaa074d40cc760b
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-05 10:02:43 -0700

    Add tests for App and configure with redux and mock store

--- src/_tests/app.test.js
+++ src/_tests/app.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import App from '../components/App';
 import fakeLocations from './testLocations';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App locations={fakeLocations} />, div);
+
+describe('App', () => {
+  const initialState = {
+    Locations: fakeLocations,
+    polygon: [],
+  };
+  const mockStore = configureStore();
+  let store;
+  let mockfetchAllLocations;
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+    mockfetchAllLocations = jest.fn();
+
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <App fetchAllLocations={mockfetchAllLocations} />
+      </Provider>,
+      div
+    );
+  });
+
+  it('renders without crashing', () => { });
+  it('Should fetch locations on mounting', () => {
+    expect(mockfetchAllLocations.mock.calls.length).toBe(1);
+  });
 });
