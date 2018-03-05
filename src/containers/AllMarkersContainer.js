c29b7dc98e3c5a79ae8d00c1bb8e267e492e3227
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 18:40:15 -0700

    Refactor AllMarkerContainer to use bindActionCreators

--- src/containers/AllMarkersContainer.js
+++ src/containers/AllMarkersContainer.js
@@ -1,15 +1,14 @@
 import { connect } from 'react-redux';
-import AllMarkers from '../components/AllMarkers';
+import { bindActionCreators } from 'redux';
 import { modifyPolygon } from '../actions';
+import AllMarkers from '../components/AllMarkers';
 
 const mapStateToProps = ({ Locations }) => {
   return { locations: Locations };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    modifyPolygon: location => dispatch(modifyPolygon(location)),
-  };
+  return bindActionCreators({ modifyPolygon }, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllMarkers);
