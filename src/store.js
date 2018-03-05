f1d73e903a7c4358e69fba368bcd065a90246051
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 18:57:19 -0700

    Rename rootReducer.js to index.js and fix imports

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import RootReducer from './reducers/RootReducer';
+import RootReducer from './reducers';
 
 const initialState = {};
 const middleWare = [thunk];
