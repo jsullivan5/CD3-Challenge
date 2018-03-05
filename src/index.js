f84f2681a0dd6f26e014cb74a8aa9afa75d2807c
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 18:03:15 -0700

    Refactor app index.js to import store

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
+
 import './index.css';
-import RootReducer from './reducers/RootReducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-  RootReducer,
-  {},
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
-)
-
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
+);
 
-  , document.getElementById('root'));
 registerServiceWorker();
