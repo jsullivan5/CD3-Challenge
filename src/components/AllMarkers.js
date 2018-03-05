4dffb3eb93839009ffd513b2950f7199057fd5bd
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-05 12:41:47 -0700

    Pass removeLocation as props to Marker in Allmarkers

--- src/components/AllMarkers.js
+++ src/components/AllMarkers.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import MapMarker from './Marker';
 
-const AllMarkers = ({ locations, modifyPolygon }) => {
+const AllMarkers = ({ locations, modifyPolygon, removeLocation }) => {
   const markerArray = locations ?
     locations.map((marker) => {
       return (
@@ -13,6 +13,8 @@ const AllMarkers = ({ locations, modifyPolygon }) => {
             marker.lat,
             marker.lng,
           ])}
+          removeLocation={removeLocation}
+          marker={marker}
         />
       );
     })
