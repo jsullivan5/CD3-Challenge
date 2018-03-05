6d7cf749fcfb39436c696053579309d22919773e
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 18:34:27 -0700

    Refactor Marker to stateless component

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Marker, Tooltip } from 'react-leaflet';
 import L from 'leaflet';
 import tealdot from '../imgs/tealdot.svg';
 
-class MapMarker extends Component {
+const MapMarker = ({ location, onClick, name }) => {
+  const icon = L.icon({
+    iconUrl: tealdot,
+    iconSize: [15, 15],
+  });
 
-  render() {
-    const icon = L.icon({
-      iconUrl: tealdot,
-      iconSize: [15, 15],
-    });
-
-    return (
-      <div className="marker-container">
-        <Marker
-          position={this.props.location}
-          icon={icon}
-          onClick={this.props.onClick}
+  return (
+    <div className="marker-container">
+      <Marker
+        position={location}
+        icon={icon}
+        onClick={onClick}
+      >
+        <Tooltip
+          sticky
+          interactive
         >
-          <Tooltip
-            sticky
-            interactive
-          >
-            <div>
-              <h4>{this.props.name}</h4>
-            </div>
-          </Tooltip>
-        </Marker>
-      </div>
-    );
-  }
-}
+          <div>
+            <h4>{name}</h4>
+          </div>
+        </Tooltip>
+      </Marker>
+    </div>
+  );
+};
 
 export default MapMarker;
