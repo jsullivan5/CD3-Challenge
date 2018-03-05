db074f92d6148a816950f15070bae09d79f85c37
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-05 12:42:52 -0700

    Add popup to Marker with delete button and removeLocation on click

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Marker, Tooltip } from 'react-leaflet';
+import { Marker, Tooltip, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import tealdot from '../imgs/tealdot.svg';
 
-const MapMarker = ({ location, onClick, name }) => {
+const MapMarker = ({ location, onClick, name, removeLocation, marker }) => {
   const icon = L.icon({
     iconUrl: tealdot,
     iconSize: [15, 15],
@@ -24,6 +24,11 @@ const MapMarker = ({ location, onClick, name }) => {
             <h4>{name}</h4>
           </div>
         </Tooltip>
+        <Popup>
+          <button onClick={() => removeLocation(marker)}>
+            Delete
+          </button>
+        </Popup>
       </Marker>
     </div>
   );
