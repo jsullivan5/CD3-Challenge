618ed4cb71233b942bcf26e2a2edcb639811f000
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-04 18:28:11 -0700

    Refactor AllMarkers to stateless component

--- src/components/AllMarkers.js
+++ src/components/AllMarkers.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MapMarker from './Marker';
 
-class AllMarkers extends Component {
-  render() {
-    const { locations } = this.props;
-    const markerArray = locations ?
-      locations.map((marker) => {
-        return (
-          <MapMarker
-            key={marker.id}
-            location={[+marker.lat, +marker.lng]}
-            name={marker.name}
-            onClick={() => this.props.modifyPolygon([
-              marker.lat,
-              marker.lng,
-            ])}
-          />
-        );
-      })
-      : null;
+const AllMarkers = ({ locations, modifyPolygon }) => {
+  const markerArray = locations ?
+    locations.map((marker) => {
+      return (
+        <MapMarker
+          key={marker.id}
+          location={[+marker.lat, +marker.lng]}
+          name={marker.name}
+          onClick={() => modifyPolygon([
+            marker.lat,
+            marker.lng,
+          ])}
+        />
+      );
+    })
+    : null;
 
-    return (
-      <div className="paths-container">
-        {markerArray}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="paths-container">
+      {markerArray}
+    </div>
+  );
+};
 
 export default AllMarkers;
