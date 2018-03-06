0bca57619181985f916e03358b94cb558ad5a9fc
James Sullivan <jamesfsullivan5@gmail.com>
2018-03-05 17:27:01 -0700

    Add Error Handler to server

--- server/server.js
+++ server/server.js
@@ -20,46 +20,53 @@ async function run() {
 
   console.log('Connected to the database');
 
-  app.get('/locations', async (req, res) => {
-    try {
-      const locations = await cd3Collection.find().toArray();
-      res.status(200).send({ locations });
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  app.post('/locations', async (req, res) => {
-    const location = req.body;
-
-    try {
-      const results = await cd3Collection.insert(location);
-      res.status(201).send(results.ops[0]);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  app.delete('/locations/:id', async (req, res) => {
-    const { id } = req.params;
-
-    try {
-      const results = await cd3Collection.deleteOne({
-        _id: ObjectId(id),
-      });
-      console.log(results);
-      res.sendStatus(204);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
   app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
   // Always return the main index.html, so react-router render the route in the client
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
   });
+
+  app.get('/locations', async (req, res, next) => {
+    try {
+      const locations = await cd3Collection.find().toArray();
+      return res.status(200).send({ locations });
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.post('/locations', async (req, res, next) => {
+    const location = req.body;
+
+    try {
+      const results = await cd3Collection.insert(location);
+      return res.status(201).send(results.ops[0]);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  app.delete('/locations/:id', async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+      await cd3Collection.deleteOne({
+        _id: ObjectId(id),
+      });
+      return res.sendStatus(204);
+    } catch (error) {
+      return next(error);
+    }
+  });
+
+  // Error Handler
+  app.use((err, req, res) => {
+    console.error(err);
+    res.status(err.status || 500)
+      .send(err.message || 'Internal Server Error');
+  });
+
 
   const portNumber = process.env.PORT || 3001;
 
